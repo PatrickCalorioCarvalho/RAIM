65075e587e99da07eabf35eb34bd5e6d60e9a2da
patrick <patrick.calorio@hotmail.com>
2021-08-26 22:58:57 -0300

    add doc

--- Servico/ChatBot/main.py
+++ Servico/ChatBot/main.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 import telebot
 import time
 
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot('1959854630:AAGyY9TQekQ47x0ZgCV9FhAbV_X_0HrC-CA')
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Veja Ultimas Leituras /photo")
+	bot.reply_to(message, "Veja Ultimas Leituras /photo \n Pegar Ultimas Leituras /doc")
 
 @bot.message_handler(commands=['photo'])
 def send_photo(message):
@@ -27,6 +27,24 @@ def send_photo(message):
     img = open(nomePhoto, 'rb')
     bot.send_photo(message.chat.id, img, reply_to_message_id=message.message_id)
     img.close()
+    mydb.close()
+
+@bot.message_handler(commands=['doc'])
+def send_doc(message):
+    mydb = mysql.connector.connect(
+      host="192.168.18.249",
+      user="pi",
+      password="raspberry",
+      database="RAIM"
+    )
+    sql = "SELECT Temperatura,Umidade,Pressao,DataLeitura FROM LeituraMeteorologica"
+    DFLeituraMeteorologica = pd.read_sql(sql, mydb)
+    nomePhoto = ("myplot"+str(time.time())+".csv")
+    DFLeituraMeteorologica.to_csv (nomePhoto, index = False, header=True)
+    bot.send_chat_action(message.chat.id, 'upload_document')
+    doc = open(nomePhoto, 'rb')
+    bot.send_document(message.chat.id, doc, reply_to_message_id=message.message_id)
+    doc.close()
     mydb.close()
 
 bot.polling()
